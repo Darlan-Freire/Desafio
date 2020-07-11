b4ff0558cd68426a74c1fae10ac8ff1358ec61a8
darlan.freire <darlan_freirerj@hotmail.com>
2020-07-11 01:05:16 -0300

    ajuste no modelo

--- models/gradeModel.js
+++ models/gradeModel.js
@@ -23,7 +23,7 @@ export default (mongoose) => {
   });
 
   schema.method('toJSON', function () {
-    const { __v, __id, ...object } = this.toObject();
+    const { __v, _id, ...object } = this.toObject();
     object.id = _id;
     return object;
   });
